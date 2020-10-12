02e89ee1b068678e85c499c1b7022aad6a6eb6ef
HolgerZerbe <holger.zerbe@web.de>
2020-10-12 15:59:58 +0200

    Lecture 11 started

---
+++ 11_lektion11/hallo.php
@@ -0,0 +1,36 @@
+<?php 
+if (!isset($_POST["vorname"]) || !isset($_POST["nachname"])) {
+  $ausgabe = "Wer sind sie?";
+} else {
+  $ausgabe = "Hallo " . ucfirst($_POST["vorname"]) . " " . ucfirst($_POST["nachname"]) . ". Schön, dass Sie wieder da sind.";
+}
+
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>hallo.php</title>
+
+    <form action="<?PHP echo $_SERVER['PHP_SELF']; ?> " method="post"> 
+
+
+    <input type="text" name="vorname" id="vorname" placeholder="Vorname" required>
+    <input type="text" name="nachname" id="nachname" placeholder="Nachname" required>  
+
+    <input type="submit" value="Absenden">
+  
+  </form>   
+
+  <p>Hallo <?php echo $ausgabe ?></p>
+</head>
+<body>
+    
+</body>
+</html>
