c82926e5f547b80bb8a4a272fc5c7406980c67c3
HolgerZerbe <holger.zerbe@web.de>
2020-10-22 11:23:30 +0200

    lecture 22 continued

--- 22_Array-Tricks/eigen.php
+++ 22_Array-Tricks/eigen.php
@@ -1,12 +1,19 @@
 <?php
 
-function ueberpruefeObDatentypIdentisch($a, $b) {
+function pruefFunktion($a, $b) {
 
     return $b - $a;
 
 }
 
-$testArrayNumbers = [23, 4711, 94, 17, 15, 666];
+function sortiereNamen($a, $b) {
+
+    return [$a["nachname"], $a["vorname"]] <=> [$b["nachname"], $b["vorname"]];
+
+}
+
+$testArrayNumbers_1 = [23, 4711, 94, 17, 15, 666];
+$testArrayNumbers_2 = [23, 4711, 94, 17, 15, 666];
 $testArrayStrings = ["Berlin", "Koblenz", "New York", "Köln", "NEW BERLIN"];
 $testArrayMixed = [23, "Berlin", "New Berlin", 17, 15, "NEW BERLIN"];
 $testArrayMixedForAsort = [23, "Berlin", "New Berlin", 17, 15, "NEW BERLIN"];
@@ -33,7 +40,7 @@ $testArrayMixedForUsort = [23, "Berlin", "New Berlin", 17, 15, "NEW BERLIN"];
 
 
 
-echo "<h3>sort()</h3>"; 
+echo "<h3>sort()</h3>";
 
 echo 'sort(array) sortiert die Items eines Arrays nach ihren Werten gemäß folgenden Regeln:<br>
 Groß-Buchstaben vor Klein-Buchstaben vor Ziffern<br><br>
@@ -41,9 +48,9 @@ Groß-Buchstaben vor Klein-Buchstaben vor Ziffern<br><br>
 Dabei wird der ursprüngliche Array überschrieben! Es werden neue Indizes vergeben<br><br>
 
 
-$testArrayNumbers = [23, 4711, 94, 17, 15, 666] ergibt: <br>';
-sort($testArrayNumbers);
-echo var_dump($testArrayNumbers) . "<br><br>";
+$testArrayNumbers_1 = [23, 4711, 94, 17, 15, 666] ergibt: <br>';
+sort($testArrayNumbers_1);
+echo var_dump($testArrayNumbers_1) . "<br><br>";
 
 echo '$testArrayStrings = ["Berlin", "Koblenz", "New York", "Köln", "NEW BERLIN"] ergibt:<br>';
 sort($testArrayStrings);
@@ -53,7 +60,7 @@ echo '$testArrayMixed = [23, "Berlin", "New Berlin", 17, 15, "NEW BERLIN"] ergib
 sort($testArrayMixed);
 echo var_dump($testArrayMixed) . "<br><br>";
 
-echo "<h3>rsort()</h3>"; 
+echo "<h3>rsort()</h3>";
 
 echo 'rsort() funktioniert äquivalent zu sort(), nur gibt es einen <b>reversed</b> Array zurück:<br>';
 echo '$testArrayMixedReverse = [23, "Berlin", "New Berlin", 17, 15, "NEW BERLIN"] ergibt:<br>';
@@ -74,7 +81,7 @@ sort($testArrayStringsMitBildernamen , SORT_NATURAL);
 echo var_dump($testArrayStringsMitBildernamen) . "<br><br>";
 
 
-echo "<h3>asort()</h3>"; 
+echo "<h3>asort()</h3>";
 
 echo 'asort() sortiert die Werte eines Arrays, vergibt aber keine neuen Indizes!!!<br>';
 
@@ -104,7 +111,7 @@ asort($testArrayAssoziativ, SORT_STRING | SORT_FLAG_CASE);
 
 echo var_dump($testArrayAssoziativ). "<br><br>";
 
-echo "<h3>ksort()</h3>"; 
+echo "<h3>ksort()</h3>";
 
 echo 'ksort() sortiert den Array nach den Indizes eines Arrays:<br>';
 
@@ -114,21 +121,80 @@ echo '$testArrayAssoziativForKsort = ["voname"=> "Donald", "nachname"=> "Duck", 
 ksort($testArrayAssoziativForKsort);
 echo var_dump($testArrayAssoziativForKsort) . "<br>";
 
-echo "<h3>usort(), uasort(), uk(sort)</h3>"; 
+echo "<h3>usort(), uasort(), uk(sort)</h3>";
 
 echo 'Mit diesen drei Methoden können Arrays mit <b>U</b>ser-definierten Funktionen sortiert werden (analag zu sort(), asort und ksort()<br>
 die Suchfunktion muss vorher definiert sein und wird als Callback-Funktion als zweiter Parameter übergeben.<br><br>';
 
+echo'Gegeben ist folgende Prüffunktion:<br>
+function pruefFunktion($a, $b) {
 
-foreach ($testArrayMixedForUsort as $value) {
-    
-    echo gettype($value);
-    
+    return $b - $a;
+
+}
+<br><br>';
+echo 'und folgender Array $testArrayNumbers_2 = [3, 4711, 94, 17, 15, 666] <br><br>';
+
+usort($testArrayNumbers_2, 'pruefFunktion');
+
+echo 'usort($testArrayNumbers_2, "pruefFunktion") ergibt somit: <br>';
+
+echo var_dump($testArrayNumbers_2) . "<br><br>";
+
+echo "Hier macht also die Prüffunktion das gleiche wir rsort()";
+
+
+echo "<h3>Spaceship-Operator <=></h3>";
+
+echo 'Der Spaceship-Operator ersetzt die Funktion ($a < $b) ? -1 : (($a > $b) ? 1 : 0) und hilft bei der Sortierung von Strings. <br><br>';
+echo 'Besonders nützlich ist er z.B. bei der Sortierung von mehrdimensionalen Arrays:<br><br>';
+
+echo'Gegeben ist folgende Prüffunktion:<br>
+function sortiereNamen($a, $b) {
+
+    return [$a["nachname"], $a["vorname"]] <=> [$b["nachname"], $b["vorname"]];
+
+}
+<br><br>';
+
+$personenArray = [
+    ["vorname" => "Else", "nachname" => "Schmidt"],
+    ["vorname" => "Jakob", "nachname" => "Maier"],
+    ["vorname" => "Elisabeth", "nachname" => "Schmidt"],
+    ["vorname" => "Ottfried", "nachname" => "Maierle"],
+];
+
+echo 'und folgender Array $personenArray: <br><br>';
+?>
+<pre><?php var_dump($personenArray) ?></pre>;
+
+<?php
+
+echo '<br>ergibt nach usort(personenArray, "sortiereNamen") und dem Ausführen einer For-Schleife <br>
+bei der auf jedes Item ksort() ausgeführt wurde: <br><br>';
+
+usort($personenArray, "sortiereNamen");
+for ($i = 0; $i<count($personenArray); $i++) {
+    ksort($personenArray[$i]);
 }
 
-$newArray = usort($testArrayMixedForUsort, 'ueberpruefeObDatentypIdentisch');
-echo var_dump($testArrayMixedForUsort) . "<br>";
-echo var_dump($newArray) . "<br>";
+?>
+<pre><?php var_dump($personenArray) ?></pre>;
 
+<?php 
+echo "<h3>Anpnyme Funktionen</h3>";
+
+echo 'Anstatt eines Funktionsnamens kann auch eine anonyme Funktion als Callback übergeben werden,<br>';
+echo 'dies ist besonders nützlich bei kurzen Prüffunktionen, elche nicht nochmal verwendet werden:<br><br>';
+
+echo'
+$zahlen = [4711, 63, -111, 0 , 24, -201]; ergibt somit: <br>
+usort($zahlen, fn ($a, $b) => $a <=> $b);<br>
+// wichtig ist in PHP hier das Schlüsselwort <b>fn</b> für eine anonyme Funktion!
+
+<br><br>';
+$zahlen = [4711, 63, -111, 0 , 24, -201];
+usort($zahlen, fn ($a, $b) => $a <=> $b);
+echo var_dump($zahlen);
 
 ?>
