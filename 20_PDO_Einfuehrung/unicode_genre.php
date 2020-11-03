49dc84b4735d8b32793302014fba770ed5959b3a
HolgerZerbe <holger.zerbe@web.de>
2020-11-03 11:53:07 +0100

    lecture 20 finished

--- 20_PDO_Einfuehrung/unicode_genre.php
+++ 20_PDO_Einfuehrung/unicode_genre.php
@@ -4,20 +4,20 @@ require_once 'inc/datenbank.inc.php';
 
 
 $db->query(
-    'CREATE TABLE genre (
+    'CREATE TABLE genres (
         id INT NOT NULL auto_increment PRIMARY KEY,
         titel VARCHAR(255)
         ) default CHARSET = utf8'
 );
 
 
-$db->query('INSERT INTO genre (titel) VALUES ("Rocky V")');
+$db->query('INSERT INTO genres (titel) VALUES ("Rocky V")');
 
 $statement = $db->query('SELECT * FROM genre');
 $daten = $statement->fetchAll();
 
 echo var_dump($daten);
 
-$db->query('DROP TABLE genre');
+$db->query('DROP TABLE genres');
 
 ?>
